ab249f84c7f73e2562bbf4ac523fd83bc2d536ab
acse-cz1620 <72449047+acse-cz1620@users.noreply.github.com>
2021-07-09 22:16:11 +0800

    Update efficientdet_test.py

--- efficientdet_test.py
+++ efficientdet_test.py
@@ -15,12 +15,12 @@ import numpy as np
 from efficientdet.utils import BBoxTransform, ClipBoxes
 from utils.utils import preprocess, invert_affine, postprocess, STANDARD_COLORS, standard_to_bgr, get_index_label, plot_one_box
 
-compound_coef = 0
+compound_coef = 5
 force_input_size = None  # set None to use default size
-img_path = 'test/img.png'
+img_path = '/content/Yet-Another-EfficientDet-Pytorch/datasets/craters/val/oxiapalus_29_9.png'
 
 # replace this part with your project's anchor config
-anchor_ratios = [(1.0, 1.0), (1.4, 0.7), (0.7, 1.4)]
+anchor_ratios = [(0.9, 1.1), (1.0, 1.0), (1.1, 0.9)]
 anchor_scales = [2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)]
 
 threshold = 0.2
@@ -31,16 +31,7 @@ use_float16 = False
 cudnn.fastest = True
 cudnn.benchmark = True
 
-obj_list = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-            'fire hydrant', '', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
-            'cow', 'elephant', 'bear', 'zebra', 'giraffe', '', 'backpack', 'umbrella', '', '', 'handbag', 'tie',
-            'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
-            'skateboard', 'surfboard', 'tennis racket', 'bottle', '', 'wine glass', 'cup', 'fork', 'knife', 'spoon',
-            'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut',
-            'cake', 'chair', 'couch', 'potted plant', 'bed', '', 'dining table', '', '', 'toilet', '', 'tv',
-            'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink',
-            'refrigerator', '', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
-            'toothbrush']
+obj_list = ['crater']
 
 
 color_list = standard_to_bgr(STANDARD_COLORS)
